d6ed71b40bed79199387f87ec8115be7e5398c7b
Zhongcai Ng <ngzhongcai@hotmail.com>
2015-12-14 00:34:51 +0800

    Cloudilly / NodeJS
    Cloudilly / NodeJS

---
+++ app.js
@@ -0,0 +1,34 @@
+var app= "<INSERT YOUR APP IDENTIFIER>";
+var access= "<INSERT YOUR ACCESS KEY>";
+var cloudilly= require("/Cloudilly/hook.min.js");
+
+cloudilly.initialize(app, access, function() {
+	cloudilly.connect();
+});
+
+cloudilly.socketConnected(function(err, res) {
+	if(err) { error("ERROR: SocketConnected"); return; }
+	console.log("SocketConnected");
+	
+	cloudilly.listen("public", function(err, res) {
+		if(err) { console.log("ERROR: Listen"); return; }
+		return;
+	});
+});
+
+cloudilly.socketDisconnected(function() {
+	console.log("SocketDisconnected");
+});
+
+cloudilly.socketReceivedDevice(function(res) {
+	console.log("@@@@@@ RECEIVED DEVICE");
+	console.log(res);
+	
+	// DO SOMETHING INTERESTING HERE
+	// IE, SEND OUT PUSH NOTIFICATION IF SOMEONE ENTERS PUBLIC
+});
+
+cloudilly.socketReceivedPost(function(res) {
+	console.log("@@@@@@ RECEIVED POST");
+	console.log(res);
+});
